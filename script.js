81a4deadff50ad83e378cd9fb607ad91357f345e
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-06 20:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+const words=Array.from(document.getElementsByTagName("strong"))
 function highlight() {
     //Write your code here
-
+words.forEach((w)=>{
+	w.style.color="green";
+})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+words.forEach((w)=>{
+	w.style.color="black";
+})
     
 }
